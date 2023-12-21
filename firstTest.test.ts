59159be51c8a83785ce6551074d666e9b582c4a4
Hunter <huntergerrard7@gmail.com>
2023-12-20 19:35:48 -0700

    Second Push

--- firstTest.test.ts
+++ firstTest.test.ts
@@ -2,9 +2,18 @@ import { Hunter } from "./hunterPage";
 const page = new Hunter(); 
 
 
-test("Click on the FAQ Page", async () => {
+test("can click cookie", async () => {
     await page.navigate();
-    await page.click(page.faqlist);
-    await page.driver.quit();
+    await page.driver.manage().window();
+    await page.click(page.engButton);
+    await page.driver.sleep(1000);
+    await page.click(page.bigCookie);
+    await page.repeatClick(80,page.bigCookie);
+    await page.click(page.upgradeButton);
+    await page.click(page.upgradeButton2);
+    await page.driver.sleep(1000);
+    await page.click(page.twitter);
+    await page.tabs();
+
     
-})
+});
--- hunterPage.ts
+++ hunterPage.ts
@@ -1,11 +1,36 @@
 import{By}from 'selenium-webdriver';
 import{BasePage}from './basePage';
+const fs = require('fs');
 
 export class Hunter extends BasePage{
-    faqlist: By = By.xpath('//*[@id="footer"]/div[1]/div/dl[4]/dd/ul/li[2]/a');
+    englishButton: By = By.xpath('//*[@id="langSelect-EN"]');
+    engButton:By=By.id('langSelect-EN');
+    bigCookie:By=By.id('bigCookie');
+    twitter:By=By.xpath('//*[@id="topbarTwitter"]');
+    statsButton:By=By.xpath('//*[@id="statsButton"]')
+    upgradeButton:By=By.xpath('//*[@id="product0"]')
+    upgradeButton2:By=By.xpath('//*[@id="upgrade0"]')
 
 
     constructor(){
-        super({url:'https://www.oneplus.com/us'});
+        super({url:'https://orteil.dashnet.org/cookieclicker/'});
+    };
+    async repeatClick(num, clickThing) {
+        for (let i = 0; i < num; i++) {
+            await this.click(clickThing); 
+        };
+    };
+    async tabs(){
+        let myTabs=await this.driver.getAllWindowHandles();
+        await this.driver.switchTo().window(myTabs[1]);
+        await this.driver.sleep(1500);
+        fs.writeFile(`${__dirname}/xPicture.png`,
+        await this.driver.takeScreenshot(),"base64",
+        (e)=>{
+            if (e)console.log(e);
+            else console.log("A picture is worht a thousand words");
+        });
+        await this.driver.close();
+        await this.driver.switchTo().window(myTabs[0]);
     };
 };
