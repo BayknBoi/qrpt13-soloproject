b7e3e129211e4d388c4e0fe4e1b16ce9c380b284
Hunter <huntergerrard7@gmail.com>
2024-01-02 17:59:54 -0700

    push

--- firstTest.test.ts
+++ firstTest.test.ts
@@ -6,14 +6,14 @@ test("can click cookie", async () => {
     await page.navigate();
     await page.driver.manage().window();
     await page.click(page.engButton);
-    await page.driver.sleep(1000);
+    await page.driver.sleep(2000);
     await page.click(page.bigCookie);
     await page.repeatClick(80,page.bigCookie);
     await page.click(page.upgradeButton);
     await page.click(page.upgradeButton2);
-    await page.driver.sleep(1000);
+    await page.driver.sleep(1000)
     await page.click(page.twitter);
     await page.tabs();
-
+    await page.driver.quit();
     
-});
+})
